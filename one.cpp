0e0d6fed9144f2403bf31b3e71fff1f8dea369bb
champtiravat <tiravat2016@gmail.com>
2019-03-01 02:13:16 +0700

    :sparkles: Added some code!

---
+++ one.cpp
@@ -0,0 +1,12 @@
+#include <iostream> // Allow us to use count and more 
+#include <vector> // Arrays
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello, World" << endl;
+
+  return 0;
+}
